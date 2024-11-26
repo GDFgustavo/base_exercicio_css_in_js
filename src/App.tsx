40245f9f2a9df112ebf7ce1eed72378a052fc5c8
Gustavo GDF <gustavoaguiar398@gmail.com>
2024-11-26 15:36:45 -0300

    Convertendo a estilização do projeto, para o uso do styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <div className="container">
+      <EstiloGlobal></EstiloGlobal>
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,16 @@
-import styles from './Hero.module.css'
+import { Form, Titulo, ContainerTitulo } from './styles'
+import { Container } from '../../styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <Form>
+    <Container>
+      <ContainerTitulo>
+        <Titulo>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </Titulo>
+      </ContainerTitulo>
+    </Container>
+  </Form>
 )
 
 export default Hero
